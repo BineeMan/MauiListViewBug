c4561917a0f1334642a629b88594886f537e1b4e
BineeMan <103749855+BineeMan@users.noreply.github.com>
2023-03-28 19:57:42 +0700

    Initial commit

---
+++ MauiApp4/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using MauiApp4.Model;
+
+namespace MauiApp4;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		BindingContext = new MainPageViewModel();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+
+	}
+}
+
---
+++ MauiApp4/Model/MainPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp4.Model
+{
+    public partial class MainPageViewModel : ObservableObject
+    {
+        public MainPageViewModel()
+        {
+            monkeys = new ObservableCollection<string>();
+            monkeys.Add("Monk");
+            monkeys.Add("Monk2");
+        }
+
+        [ObservableProperty]
+        ObservableCollection<string> monkeys;
+
+        [RelayCommand]
+        void Remove(string key)
+        {
+            Debug.WriteLine(key);
+        }
+
+    }
+}
